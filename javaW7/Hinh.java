b94d3ff0b85ab5e9e7bcf0d23be84d89ec95545e
its-hmtu <tu.hoangminh15@gmail.com>
2023-03-07 17:12:26 +0700

    New commit

---
+++ javaW7/Hinh.java
@@ -0,0 +1,16 @@
+package javaW7;
+
+public class Hinh {
+    private double p, s;
+    private Point point;
+    final double PI = 3.14;
+    public Hinh() {}
+
+    public Hinh(Point point, double p, double s) {
+        
+        this.p = p;
+        this.s = s;
+    }
+
+
+}
